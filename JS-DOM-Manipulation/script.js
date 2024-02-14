d3e1e49a5c2a17933812b0fbfc84db36adc9e2b7
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2024-02-14 16:33:35 +0530

    ok

--- JS-DOM-Manipulation/script.js
+++ JS-DOM-Manipulation/script.js
@@ -12,16 +12,44 @@
 // let itemOne_1 = document.querySelector('#item-1');
 // console.log(itemOne_1);
 
-let itemList2 = document.querySelectorAll('.item');
-console.log(itemList2);
+// let itemList2 = document.querySelectorAll('.item');
+// console.log(itemList2);
 
-itemList2.forEach((element)=>{
-    console.log(element);
-});
+// itemList2.forEach((element)=>{
+//     console.log(element);
+// });
 
-let itemList2Arr1 = Array.from(itemList2)
-console.log(itemList2Arr1);
+// let itemList2Arr1 = Array.from(itemList2)
+// console.log(itemList2Arr1);
 
-let itemList2Arr2 = [...itemList2];
-console.log(itemList2Arr2);
+// let itemList2Arr2 = [...itemList2];
+// console.log(itemList2Arr2);
+
+let parent = document.querySelector('.item-container');
+// let children = parent.childNodes;
+// console.log(children);
+
+// let child= document.querySelector('.item-one');
+// // let parent = child.parentNode;
+// // console.log(parent);
+
+// let parent = child.closest('.item-container');
+// console.log(parent);
+
+// let newHeading = document.createElement("h1");
+// let newHeadingText = document.createTextNode("This is my title");
+// newHeading.append(newHeadingText);
+// document.body.append(newHeading);
+// parent.style.border = "1px solid blue";
+
+// parent.insertBefore(newHeading, item)
+
+
+// console.log(document.getElementById("demopara").innerText);
+// console.log(document.getElementById("demopara").textContent);
+// console.log(document.getElementById("demopara").innerHTML);
+
+
+item-one.classList.add('hello')
+// itemOne.classList.remove('hello')
 
