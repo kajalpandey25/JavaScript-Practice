90a250e67aa29fd110109cb888bbc13220b6f9b1
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2024-02-19 16:20:47 +0530

    JS-Practice

--- Array-Method/note.md
+++ Array-Method/note.md
@@ -24,3 +24,28 @@ The arr.splice method is a swiss army knife for arrays. It can do everything: in
 The syntax is:
 arr.splice(start[, deleteCount, elem1, ..., elemN])
 It modifies arr starting from the index start: removes deleteCount elements and then inserts elem1, ..., elemN at their place. Returns the array of removed elements.
+
+
+Slice =>
+The method arr.slice is much simpler than similar-looking arr.splice.
+
+The syntax is:
+arr.slice([start], [end])
+
+It returns a new array copying to it all items from index start to end (not including end). Both start and end can be negative, in that case position from array end is assumed.
+
+It’s similar to a string method str.slice, but instead of substrings it makes subarrays.
+
+
+
+Concat => 
+The method arr.concat creates a new array that includes values from other arrays and additional items.
+
+The syntax is:
+arr.concat(arg1, arg2...)
+
+It accepts any number of arguments – either arrays or values.
+
+The result is a new array containing items from arr, then arg1, arg2 etc.
+
+If an argument argN is an array, then all its elements are copied. Otherwise, the argument itself is copied.
--- Array-Method/script.js
+++ Array-Method/script.js
@@ -1,32 +1,32 @@
 
 // Array 
 
-// let arr = [1,2,3,4,5];
+let arr = [1,2,3,4,5];
 
-// console.log(arr);  // find the whole elements
-// console.log(arr[0]); // find the 1st elements 
-// console.log(arr[4]); // find the last elements
-// console.log(arr.length);  //find the length of array
+console.log(arr);  // find the whole elements
+console.log(arr[0]); // find the 1st elements 
+console.log(arr[4]); // find the last elements
+console.log(arr.length);  //find the length of array
 
-// arr.push(6);
-// console.log(arr.push(6));
-// arr.pop();
-// arr.shift()
-// arr.unshift(0)
-// console.log(arr);
+arr.push(6);
+console.log(arr.push(6));
+arr.pop();
+arr.shift()
+arr.unshift(0)
+console.log(arr);
 
 
 // How to delete an elements from the array?
 // The array are objects, so we can try to use delete:
 
-// let Arr = ["I", "Go", "Home"];
-// delete Arr[1]; // remove "go"
-// console.log(Arr);
+let Arr = ["I", "Go", "Home"];
+delete Arr[1]; // remove "go"
+console.log(Arr);
 
-// alert(Arr[1]) ; // undefined
+alert(Arr[1]) ; // undefined
 
-// // now Arr = ["I", "Home"];
-// alert(Arr.length); //3
+// now Arr = ["I", "Home"];
+alert(Arr.length); //3
 
 // We don't use delete method in array because the elements was removed , but the array still has 3 elements, we can see that arr.length == 3.
 
@@ -39,57 +39,57 @@
 // Let’s start with the deletion:
 
 // example-1
-// let arr2 = [10,20,30,40,50,60];
+let arr2 = [10,20,30,40,50,60];
 
-// arr2.splice(1, 2); // from index 1 remove 2 element
-// alert( arr2 ); // [10,40,50,60]
-// console.log(arr2);
+arr2.splice(1, 2); // from index 1 remove 2 element
+alert( arr2 ); // [10,40,50,60]
+console.log(arr2);
 
-// example -2
-// let arr3 = ["I", "study", "JavaScript", "right", "now"];
+example -2
+let arr3 = ["I", "study", "JavaScript", "right", "now"];
 
-// // remove 3 first elements and replace them with another
-// arr3.splice(0, 3, "Let's", "dance");
+// remove 3 first elements and replace them with another
+arr3.splice(0, 3, "Let's", "dance");
 
-// alert( arr3 ) // now ["Let's", "dance", "right", "now"]
-// console.log(arr3);
+alert( arr3 ) // now ["Let's", "dance", "right", "now"]
+console.log(arr3);
 
-// example-3
-// let arr4 = [0,1,2,3,4,5];
-// arr4.splice(1,2, "kajal", "rani", "sonam");
-// alert(arr4);
-// console.log(arr4);
+example-3
+let arr4 = [0,1,2,3,4,5];
+arr4.splice(1,2, "kajal", "rani", "sonam");
+alert(arr4);
+console.log(arr4);
 
 // Here we can see that splice returns the array of removed elements:
 
-// let Array = ["I", "study", "JavaScript", "right", "now"];
+let Array = ["I", "study", "JavaScript", "right", "now"];
 
-// // remove 2 first elements
-// let removed = Array.splice(0, 2);
+// remove 2 first elements
+let removed = Array.splice(0, 2);
 
-// alert( removed ); // "I", "study" <-- array of removed elements
-// console.log(Array);
+alert( removed ); // "I", "study" <-- array of removed elements
+console.log(Array);
 
 // The splice method is also able to insert the elements without any removals. For that we need to set deleteCount to 0:
 
 // example-1
-// let Array2 = ["I", "study", "JavaScript"];
+let Array2 = ["I", "study", "JavaScript"];
 
-// // from index 2
-// // delete 0
-// // then insert "complex" and "language"
-// Array2.splice(2, 0, "complex", "language");
+// from index 2
+// delete 0
+// then insert "complex" and "language"
+Array2.splice(2, 0, "complex", "language");
 
-// alert( Array2 ); // "I", "study", "complex", "language", "JavaScript"
-// console.log(Array2);
+alert( Array2 ); // "I", "study", "complex", "language", "JavaScript"
+console.log(Array2);
 
 
 // example-2
 
-// let enjoy = ["I", "Am", "Going", "To","Shopping", "Mall"];
-// enjoy.splice(4,0, "Very", "Big");
-// alert(enjoy);
-// console.log(enjoy);
+let enjoy = ["I", "Am", "Going", "To","Shopping", "Mall"];
+enjoy.splice(4,0, "Very", "Big");
+alert(enjoy);
+console.log(enjoy);
 
 
 // Negative indexes allowed:-
@@ -104,3 +104,53 @@ myArr.splice(-1, 0, 3, 4);
 
 alert( myArr ); // 1,2,3,4,5
 console.log(myArr);
+
+
+
+// Slice
+// syntax=> arr.slice([start], [end])
+
+let num = [0,1,2,3,4,5];
+let num2 = num.slice(2,5);
+console.log(num2);
+
+let Num = ["t", "e", "s", "t"];
+
+alert( Num.slice(1, 3) ); // e,s (copy from 1 to 3)
+
+alert( Num.slice(-2) ); // s,t (copy from -2 till the end)
+
+
+
+// concat
+// syntax => arr.concat(arg1, arg2...)
+
+ let Num1 = [0,1,2,3,4,5];
+ let Num2 = Num1.concat(6, "seven", [8,9], [10,11,12]);
+ console.log(Num2);
+
+// Normally, it only copies elements from arrays. Other objects, even if they look like arrays, are added as a whole:
+
+let arr0 = [1, 2];
+
+let arrayLike = {
+  0: "something",
+  length: 1
+};
+
+alert( arr0.concat(arrayLike) ); // 1,2,[object , object]
+console.log(arr0);
+
+// …But if an array-like object has a special Symbol.isConcatSpreadable property, then it’s treated as an array by concat: its elements are added instead:
+
+let arr01 = [1, 2];
+
+let arrayLike1 = {
+  0: "something",
+  1: "else",
+  [Symbol.isConcatSpreadable]: true,
+  length: 2
+};
+
+alert( arr01.concat(arrayLike1) ); // 1,2,something,else
+
