11d9c9ed9d0d355488f4d484677e8267cd76c6da
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2024-02-20 14:40:32 +0530

    JS-Practice

--- Array-Method2/note.md
+++ Array-Method2/note.md
@@ -17,3 +17,55 @@ The methods arr.indexOf and arr.includes have the similar syntax and do essentia
 1. arr.indexOf(item, from) – looks for item starting from index from, and returns the index where it was found, otherwise -1.
 
 2. arr.includes(item, from) – looks for item starting from index from, returns true if found.
+
+
+find and findIndex/findLastIndex =>
+Imagine we have an array of objects. How do we find an object with the specific condition?
+
+<!-- find method  -->
+Here the arr.find(fn) method comes in handy.
+
+The syntax is:
+let result = arr.find(function(item, index, array) {
+  // if true is returned, item is returned and iteration is stopped
+  // for falsy scenario returns undefined
+});
+
+=> The function is called for elements of the array, one after another:
+
+item is the element.
+index is its index.
+array is the array itself.
+Note => If it returns true, the search is stopped, the item is returned. If nothing found, undefined is returned.
+
+
+<!-- findIndex method -->
+The arr.findIndex method has the same syntax, but returns the index where the element was found instead of the element itself. The value of -1 is returned if nothing is found.
+
+<!-- findLastIndex method -->
+The arr.findLastIndex method is like findIndex, but searches from right to left, similar to lastIndexOf.
+
+
+filter => The find method looks for a single (first) element that makes the function return true.
+
+If there may be many, we can use arr.filter(fn).
+
+The syntax is similar to find, but filter returns an array of all matching elements:
+let results = arr.filter(function(item, index, array) {
+  // if true item is pushed to results and the iteration continues
+  // returns empty array if nothing found
+});
+
+
+Transform an array =>
+Let’s move on to methods that transform and reorder an array.
+
+1. map =>
+The arr.map method is one of the most useful and often used.
+
+It calls the function for each element of the array and returns the array of results.
+
+The syntax is:
+let result = arr.map(function(item, index, array) {
+  // returns the new value instead of item
+});
--- Array-Method2/script.js
+++ Array-Method2/script.js
@@ -1,4 +1,3 @@
-
 // ----------------- Iterate:forEach-----------------
 
 // The syntax:
@@ -7,24 +6,23 @@
 // });
 
 // for each element call alert
-// ["Bilbo", "Gandalf", "Nazgul"].forEach(alert);
+["Bilbo", "Gandalf", "Nazgul"].forEach(alert);
 
 // Example-1
-// let arr = [0,1,2,3,4,5];
-// arr.forEach(function(item, index, array){
-// console.log(`${item} is at index: ${index}`);
-// });
+let arr = [0,1,2,3,4,5];
+arr.forEach(function(item, index, array){
+console.log(`${item} is at index: ${index}`);
+});
 
-// // for each element call alert
-// arr.forEach(alert);
-
+// for each element call alert
+arr.forEach(alert);
 
 // -----------------Searching in array-------------------
-let Arr = [0,1,2,3,4,5,10];
+let Arr = [0, 1, 2, 3, 4, 5, 10];
 
 // Using arr.includes
 console.log(Arr.includes(3)); // true
-console.log(Arr.includes(8)); // false 
+console.log(Arr.includes(8)); // false
 
 // Using arr.indexOf
 console.log(Arr.indexOf(1)); // 1
@@ -33,3 +31,66 @@ console.log(Arr.indexOf(100)); // -1
 // Using arr.lastIndexof
 console.log(Arr.lastIndexOf(10)); // 6
 console.log(Arr.lastIndexOf(15)); // -1
+
+// The includes method handles NaN correctly
+const Arr1 = [NaN];
+alert(Arr1.indexOf(NaN)); // -1 (wrong, should be 0)
+alert(Arr1.includes(NaN)); // true (correct)
+
+// -------------------------find and findIndex/findLastIndex-----------------------------
+
+// find method
+let users = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Pete" },
+  { id: 3, name: "Mary" },
+];
+
+let user = users.find((item) => item.id == 1);
+
+alert(user.name); // John
+
+// findIndex method
+// Find the index of the first John
+let student = [
+    {id: 1, name: "Kajal"},
+    {id: 2, name: "Lovely"},
+    {id: 3, name: "Rani"},
+    {id: 4, name: "Kajal"}
+  ];
+  
+  // Find the index of the first John
+  alert(student.findIndex(user => user.name == 'Kajal')); // 0
+  
+  // Find the index of the last John
+  alert(student.findLastIndex(user => user.name == 'Kajal')); // 3
+
+
+//   -------------------filter---------------------
+
+let emp = [
+    {id: 1, name: "Komal"},
+    {id: 2, name: "Abhi"},
+    {id: 3, name: "Khushi"}
+  ];
+  
+  // returns array of the first two users
+  let someUsers = emp.filter(item => item.id < 3);
+  
+  alert(someUsers.length); // 2
+
+//   Transform an array
+//---------------Map method  -----------------------
+
+let lengths = ["Bilbo", "Gandalf", "Nazgul"].map(item => item.length);
+alert(lengths); // 5,7,6
+
+let sqArr = [0,1,2,3,4,5].map(function(item){
+    return item*item
+});
+console.log(sqArr); // 0,1,4,9,16,25
+
+let plusOneArr = [10,20,30,40].map(function(item){
+    return item+1;
+});
+console.log(plusOneArr); // 11,21,31,41
